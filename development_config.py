aad37a99f30c5a4d30f133b2365c42c7312e8355
vincentt143 <tran.vincent17@gmail.com>
2015-04-18 14:04:53 +1000

    Remove unused configs

--- development_config.py
+++ development_config.py
@@ -1,13 +1,6 @@
 # configuration
-DATABASE = 'forte.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
 SERVER_NAME = 'localhost:5000'
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = '587'
-MAIL_USE_TLS = True
-MAIL_USE_SSL = False
-MAIL_USERNAME = 'YOUR_GMAIL_ACCOUNT'
-MAIL_PASSWORD = 'YOUR_GMAIL_PASSWORD'
